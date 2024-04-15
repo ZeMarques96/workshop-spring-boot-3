e01430863a029f8f0052c66f2c303f03ac284632
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-15 00:55:16 -0300

    OrderStatus enum

--- src/main/java/com/marques/springproject/entities/Order.java
+++ src/main/java/com/marques/springproject/entities/Order.java
@@ -26,7 +26,8 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT")
     private Instant moment;
-    private OrderStatus orderStatus;
+
+    private Integer orderStatus;
 
 
     @ManyToOne
@@ -37,9 +38,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -60,11 +62,13 @@ public class Order implements Serializable {
     }
 
     public OrderStatus getOrderStatus() {
-        return orderStatus;
+        return OrderStatus.valueOf(orderStatus);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
