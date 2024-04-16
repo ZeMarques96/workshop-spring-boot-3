3fb5184c0d800a99b8e6316bf262f0fdab0e49ff
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-15 22:50:44 -0300

    Payment one-to-one association

--- src/main/java/com/marques/springproject/config/TestConfig.java
+++ src/main/java/com/marques/springproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.marques.springproject.entities.Category;
 import com.marques.springproject.entities.Order;
 import com.marques.springproject.entities.OrderItem;
+import com.marques.springproject.entities.Payment;
 import com.marques.springproject.entities.Product;
 import com.marques.springproject.entities.User;
 import com.marques.springproject.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
--- src/main/java/com/marques/springproject/repositories/OrderRepository.java
+++ src/main/java/com/marques/springproject/repositories/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.marques.springproject.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marques.springproject.entities.Order;
